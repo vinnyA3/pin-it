829fecb9c5ed45b6ff74139c78afe92d51beacda
vinnya3 <vinaceto@yahoo.com>
2016-04-30 13:56:20 -0400

    added array brackets in directive syntax

--- public/app/directives/megadropdownDirective.js
+++ public/app/directives/megadropdownDirective.js
@@ -1,9 +1,9 @@
 module.exports = function(app){
-  app.directive('megaDropdown', function(){
+  app.directive('megaDropdown', [function(){
     return{
       restrict:'E',
       template:require('../views/partials/megadropdownPartial.html'),
       replace:true
     }
-  });
+  }]);
 }
